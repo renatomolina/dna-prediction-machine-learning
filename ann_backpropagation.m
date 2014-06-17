cadedb894f734b262e444986b2fdb46baaae6a45
Winicius Reis <darkvinny@gmail.com>
2014-06-17 09:47:18 -0300

    First version of Backpropagation

--- ann_backpropagation.m
+++ ann_backpropagation.m
@@ -1,7 +1,11 @@
-function [ output_args ] = ann_backpropagation( input_args )
+function Sigma = ann_backpropagation( A, Y,theta, L )
 %ANN_BACKPROPAGATION Summary of this function goes here
 %   Detailed explanation goes here
-
+    Sigma = zeros(Size(A));
+    Sigma(:,L) = A(:,L) - Y;
+    for l=L-1:2
+        Sigma(:,l) = (theta(:,l) * Sigma(:,l+1)) .* (A(:,l) .* (1 - A(:,l)));
+    end
 
 end
 
