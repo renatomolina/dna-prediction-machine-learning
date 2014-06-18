c6486b5d2baa5fe8cef2e1854f169c484aa776a7
Winicius Reis <darkvinny@gmail.com>
2014-06-17 21:21:22 -0300

    ANN Backpropagation with cell array

--- ann_backpropagation.m
+++ ann_backpropagation.m
@@ -1,11 +1,24 @@
-function Sigma = ann_backpropagation( A, Y,theta, L )
-%ANN_BACKPROPAGATION Summary of this function goes here
-%   Detailed explanation goes here
-    Sigma = zeros(Size(A));
-    Sigma(:,L) = A(:,L) - Y;
-    for l=L-1:2
-        Sigma(:,l) = (theta(:,l) * Sigma(:,l+1)) .* (A(:,l) .* (1 - A(:,l)));
+function sigma = ann_backpropagation( a, y,theta, L)
+    sigma = cell(1,L);
+    sigma{L} = a{L} - y;
+    for l=L-1:-1:2
+        sigma{l} = (theta{l} * sigma{l+1}) .* (a{l} .* (1-a{l}));
     end
+    
+    
+%     Sigma = zeros(size(a));
+%     y = zeros(sl,1);
+%     y(1,1) = Y;
+%     Sigma(:,L) = a(:,L) - y;
+%     for l=L-1:-1:2
+%         for j=(1:sl)
+%             a = theta(:,j,l) * Sigma(j,l+1);
+%             b = a(j,l) * (1-a(j,l));
+%             Sigma(j,l) = a * b;
+%             %Sigma(j,l) = (theta(:,j,l) * Sigma(j,l+1)) * (a(j,l) * (1-a(j,l)));
+%             %Sigma(:,l) = (theta(:,l) * Sigma(:,l+1)) .* (a(:,l) .* (1 - a(:,l)));
+%         end
+%     end
 
 end
 
