efda385cdc4561dc5a903379e21cd6d670e18c85
Renato Molina <renatomolinat@gmail.com>
2014-06-20 15:56:26 -0300

    Fixing log message of Logistic regression with regularization

--- proj04.m
+++ proj04.m
@@ -89,7 +89,7 @@ if run_logistic_reg
     %% Preparing Y for 3 rounds of Logistic Regression
     [Y_training_1, Y_training_2, Y_training_3] = logistic_data_binarization(Y_training);
     [Y_test_1, Y_test_2, Y_test_3] = logistic_data_binarization(Y_test);
-    fprintf('\nRunning Logistic Regression...\n');
+    fprintf('\nRunning Logistic Regression with Regularization...\n');
     %accuracy_logistic = 0;
     [training_accuracy_logistic1, test_accuracy_logistic1, learning_curve ]= logistic_regression_reg(X_training,Y_training_1, X_test, Y_test_1);
     plot_learning_curve(learning_curve, 'Logistic Regression with Regularization - Class EI');
